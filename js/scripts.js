803e5a28a1e020cc62027698a27779a67d644130
ajnarag25 <ajnarag25@gmail.com>
2024-03-09 15:14:22 +0800

    added starting files login and register pages

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+window.onload = displayClock();
+function displayClock() {
+    var display = new Date().toLocaleTimeString();
+    $("#LiveTime").text(display);
+    setTimeout(displayClock, 1000);
+}
